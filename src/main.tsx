44d665542113008c48ba827ae56575107182100d
Elena Karelina <karelinae03@mail.ru>
2024-06-06 20:22:32 +0700

    исправлена маршрутизация

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import { createRoot } from "react-dom/client";
 
-import { RouterProvider } from "react-router-dom";
+// import { RouterProvider } from "react-router-dom";
 import "./index.css";
-import router from "./App";
+// import router from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <>
-    <RouterProvider router={router} />
+    арарар
+    {/* <RouterProvider router={router} /> */}
   </>
 );
